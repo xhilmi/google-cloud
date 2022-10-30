1f04d21b9bd68f8b358682f7b82faefc1c1d0e21
hilmi-afifi <38133643+hilmi-afifi@users.noreply.github.com>
2022-10-30 09:12:11 +0700

    Create allow-ssh.sh

---
+++ allow-ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt install git nano vim wget curl net-tools -y;
+sudo sed -i 's/#Port .*/Port 64000/g' /etc/ssh/sshd_config;
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config;
+sudo ufw allow 64000;
+sudo ufw --force enable;
+sudo ufw reload;
+sudo service sshd restart;
+sudo ls -lah;
