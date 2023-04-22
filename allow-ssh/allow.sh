bdc2aace0f0d06a1d7ef1d89556046536531ad57
Hilmi Afifi <38133643+xhilmi@users.noreply.github.com>
2023-04-22 14:51:56 +0700

    Update allow.sh

--- allow-ssh/allow.sh
+++ allow-ssh/allow.sh
@@ -12,17 +12,18 @@ read -p "Enter your choice (1/2/3): " choice
 
 case $choice in
   1)
+    read -p "Enter the desired SSH port number: " port
     echo "Configuring SSH for CentOS..."
     sudo yum update -y;
     sudo yum provides *bin/semanage;
     sudo yum install git nano vim wget curl net-tools policycoreutils-python-utils -y;
-    sudo sed -i 's/#Port .*/Port 64000/g' /etc/ssh/sshd_config;
+    sudo sed -i "s/#Port .*/Port $port/g" /etc/ssh/sshd_config;
     sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
     sudo sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config;
     sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config;
     sudo yum install -y curl policycoreutils-python;
-    sudo semanage port -a -t ssh_port_t -p tcp 64000;
-    sudo firewall-cmd --permanent --add-port=64000/tcp;
+    sudo semanage port -a -t ssh_port_t -p tcp $port;
+    sudo firewall-cmd --permanent --add-port=$port/tcp;
     sudo firewall-cmd --reload;
     sudo systemctl enable firewalld;
     sudo systemctl restart firewalld
@@ -30,14 +31,15 @@ case $choice in
     sudo ls -lah;
     ;;
   2)
+    read -p "Enter the desired SSH port number: " port
     echo "Configuring SSH for Ubuntu/Debian..."
     sudo apt update -y;
     sudo apt install git nano vim wget curl net-tools -y;
-    sudo sed -i 's/#Port .*/Port 64000/g' /etc/ssh/sshd_config;
+    sudo sed -i "s/#Port .*/Port $port/g" /etc/ssh/sshd_config;
     sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
     sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config;
     sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config;
-    sudo ufw allow 64000;
+    sudo ufw allow $port;
     sudo ufw allow 22;
     sudo ufw --force enable;
     sudo ufw reload;
