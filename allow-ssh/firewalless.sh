494b791bed84d139dee3f0560e9409160f25dd64
Hilmi Afifi <38133643+xhilmi@users.noreply.github.com>
2023-05-06 13:56:34 +0700

    Update firewalless.sh

--- allow-ssh/firewalless.sh
+++ allow-ssh/firewalless.sh
@@ -18,9 +18,9 @@ case $choice in
     sudo yum provides *bin/semanage
     sudo yum install git nano vim wget curl net-tools policycoreutils-python-utils -y > /dev/null 2>&1
     sudo sed -i "s/.*Port .*/Port $port/g" /etc/ssh/sshd_config
-    sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-    sudo sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
-    sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+    sudo sed -i 's/.*PasswordAuthentication .*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+    sudo sed -i 's/.*PermitRootLogin .*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+    sudo sed -i 's/.*PubkeyAuthentication .*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
     sudo yum install -y curl policycoreutils-python > /dev/null 2>&1
     sudo semanage port -a -t ssh_port_t -p tcp $port
     sudo firewall-cmd --permanent --add-port=$port/tcp
@@ -38,9 +38,9 @@ case $choice in
     sudo apt update -y > /dev/null 2>&1
     sudo apt install git nano vim wget curl net-tools -y > /dev/null 2>&1
     sudo sed -i "s/.*Port .*/Port $port/g" /etc/ssh/sshd_config
-    sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-    sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
-    sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+    sudo sed -i 's/.*PasswordAuthentication .*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+    sudo sed -i 's/.*PermitRootLogin .*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+    sudo sed -i 's/.*PubkeyAuthentication .*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
     sudo ufw allow $port
     # sudo ufw --force enable
     # sudo ufw reload
